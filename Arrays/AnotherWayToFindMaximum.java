5214d24a1e02ddb8c9df4034e74e1f86daa9ef5c
Anupriya Rani <anupriyarani2002@gmail.com>
2024-07-02 20:58:13 +0530

    added AnotherWayToFindMaximum.java

---
+++ Arrays/AnotherWayToFindMaximum.java
@@ -0,0 +1,27 @@
+package Arrays;
+import java.util.Scanner;
+import java.lang.Math;
+public class AnotherWayToFindMaximum 
+{
+    public static void main(String[] args)
+     {
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter the size of array :");
+       int size = input.nextInt();
+        int arr[]= new int[size];
+
+        for(int i = 0; i < size ; i++)
+        {
+            System.out.print("Enter the element :");
+            arr[i]= input.nextInt();
+        }
+        int temp= Integer.MIN_VALUE;
+        for(int i =0; i<size; i++)
+        {
+               if(arr[i] > temp){
+                    temp=arr[i];
+               }
+        }
+        System.out.println("Largest element :"+ temp);
+    }
+}
