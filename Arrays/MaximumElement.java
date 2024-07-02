8d628a8d22aa3deb677634a95ea29bff9b8bc47d
Anupriya Rani <anupriyarani2002@gmail.com>
2024-07-02 19:26:41 +0530

    added MaximumElement.java

---
+++ Arrays/MaximumElement.java
@@ -0,0 +1,29 @@
+package Arrays;
+import java.util.Scanner;
+/*
+ * Find the maximum value out of all the elements in the array
+ */
+public class MaximumElement
+ {
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of array :");
+        int size = input.nextInt();
+        int temp =0;
+        int arr[] = new int[size];
+        
+        for(int i = 0 ; i < size ; i++)
+        {
+            System.out.print("Enter the element :");
+             arr[i] = input.nextInt();
+        }
+        for(int i = 0 ; i < size; i++)
+        {
+             if(arr[i] > temp){
+                temp = arr[i];
+             }
+        }
+        System.out.println("Maximum value out of all the elements in the array is :" + temp);
+    }
+}
